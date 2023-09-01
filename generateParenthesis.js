4474183da644de55639a4ceaaa49ff3b4dee5236
Fadly Tanjung <fadlysyah96@gmail.com>
2023-09-02 03:13:05 +0700

    fix: generateParenthesis

---
+++ generateParenthesis.js
@@ -0,0 +1,27 @@
+function generateParenthesis(n) {
+  const result= [];
+
+  const generate = (
+    str,
+    max,
+    open,
+    close,
+    result
+  ) => {
+    if (open === max && close === max) {
+      result.push(str);
+      return;
+    }
+    if (open < max) {
+      generate(str + "(", max, open + 1, close, result);
+    }
+
+    if (close < open) {
+      generate(str + ")", max, open, close + 1, result);
+    }
+  }
+  generate("", n, 0, 0, result);
+  return result;
+}
+
+console.log("Solution 1:", generateParenthesis(2));
